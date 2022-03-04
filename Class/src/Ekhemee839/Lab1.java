d51f295120dcd5299322d889d5e75cbe83de0cae
Erkhemee <erkhembayar222@gmail.com>
2022-03-04 14:48:05 +0800

    Lab 1

--- Class/src/Ekhemee839/Lab1.java
+++ Class/src/Ekhemee839/Lab1.java
@@ -1,25 +1,34 @@
 package Ekhemee839;
 import java.util.Scanner;
 public class Lab1 {
- private static final double PI = 3.14;
- private final double radius;
- public Lab1 (double radius) {
- this.radius = radius;
- }
- public static void main(String[] args) {
- System.out.println("------------------------------------------------------------");
- System.out.println("CircleCalc v1.0");
- System.out.println();
- System.out.println("Calculates and prints information for a user-supplied radius");
- System.out.println("------------------------------------------------------------");
- System.out.print("Enter the circle's radius: ");
- Scanner in = new Scanner(System.in);
-
- double r = in.nextDouble();
- double A;
- A= PI*r*r;
- System.out.printf("Area of "+ r + " is " + A);
- System.out.printf("Just checking...");
- in.close();
- }
+	public static String Utga(String[] args){
+		Scanner myObj = new Scanner(System.in);
+		String A = myObj.nextLine();
+		return A;
+	}
+	
+	public static int tsalin(String[] args) {
+		Scanner myObj = new Scanner(System.in);
+		int ts = myObj.nextInt();
+		return ts;
+	}
+	
+	public static double jTsalin(int ts) {
+		double total = ts*12/0.11;
+		return total;
+	}
+	
+	public static void main(String[] args) {
+		int ts;
+		double total;
+		System.out.println("Та нэрээ оруулна уу?\n");
+		String name = Utga(null);
+		
+		System.out.println("Та сарын цалингаа оруулна уу?\n");
+		ts = tsalin(null);
+		
+		total = jTsalin(ts);
+		
+		System.out.println("Таны гар дээрээ авах цалин = "+ total);
+	}
 }
