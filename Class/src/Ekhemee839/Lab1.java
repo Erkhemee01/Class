e8225873e3cce8eaeffca6200ddbc28d7e7012c1
Erkhemee <erkhembayar222@gmail.com>
2022-03-04 14:59:18 +0800

    Алдаа засав

--- Class/src/Ekhemee839/Lab1.java
+++ Class/src/Ekhemee839/Lab1.java
@@ -14,21 +14,22 @@ public class Lab1 {
 	}
 	
 	public static double jTsalin(int ts) {
-		double total = ts*12/0.11;
+		double total = ts*12-(ts*12*0.26);
 		return total;
 	}
 	
 	public static void main(String[] args) {
 		int ts;
-		double total;
-		System.out.println("Та нэрээ оруулна уу?\n");
+		double tur, total;
+		
+		System.out.println("Та нэрээ оруулна уу?");
 		String name = Utga(null);
 		
-		System.out.println("Та сарын цалингаа оруулна уу?\n");
+		System.out.println("Та сарын цалингаа оруулна уу?");
 		ts = tsalin(null);
 		
 		total = jTsalin(ts);
 		
-		System.out.println("Таны гар дээрээ авах цалин = "+ total);
+		System.out.println(name +" таны жилд гар дээрээ авах цалин = "+ total + "₮");
 	}
 }
