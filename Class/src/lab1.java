165e8f7c39386474076c9192b7dae8be3dd247dc
Erkhemee <erkhembayar222@gmail.com>
2022-03-04 13:27:01 +0800

    тойргийн талбай олох

---
+++ Class/src/lab1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class lab1 {
+ private static final double PI = 3.14;
+ private final double radius;
+ public lab1 (double radius) {
+ this.radius = radius;
+ }
+ public static void main(String[] args) {
+ System.out.println("------------------------------------------------------------");
+ System.out.println("CircleCalc v1.0");
+ System.out.println();
+ System.out.println("Calculates and prints information for a user-supplied radius");
+ System.out.println("------------------------------------------------------------");
+ System.out.print("Enter the circle's radius: ");
+ Scanner in = new Scanner(System.in);
+
+ double r = in.nextDouble();
+ double A;
+ A= PI*r*r;
+ System.out.printf("Area of "+ r + " is " + A);
+ in.close();
+ }
+}
