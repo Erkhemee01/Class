8367b560c5d9f6b297c4dfede44fe3e8873c5b29
Erkhemee <erkhembayar222@gmail.com>
2022-03-18 13:50:37 +0800

    Circle Calculator

--- Class/src/Ekhemee839/Lab1.java
+++ Class/src/Ekhemee839/Lab1.java
@@ -1,35 +1,35 @@
 package Ekhemee839;
 import java.util.Scanner;
-public class Lab1 {
-	public static String Utga(String[] args){
-		Scanner myObj = new Scanner(System.in);
-		String A = myObj.nextLine();
-		return A;
-	}
-	
-	public static int tsalin(String[] args) {
-		Scanner myObj = new Scanner(System.in);
-		int ts = myObj.nextInt();
-		return ts;
-	}
-	
-	public static double jTsalin(int ts) {
-		double total = ts*12-(ts*12*0.26);
-		return total;
-	}
-	
-	public static void main(String[] args) {
-		int ts;
-		double tur, total;
-		
-		System.out.println("Та нэрээ оруулна уу?");
-		String name = Utga(null);
-		
-		System.out.println("Та сарын цалингаа оруулна уу?");
-		ts = tsalin(null);
-		
-		total = jTsalin(ts);
-		
-		System.out.println(name +" таны жилд гар дээрээ авах цалин = "+ total + "₮");
-	}
+public class Lab1{
+	 private static final double PI = 3.14;
+	 private final double radius = 0;
+	 
+	 public void Circle(double radius) {
+	 this.radius = radius;
+	 }
+	 
+	 public double calcArea() {
+		 return PI*radius*radius;
+	 }
+	 
+	 public double calcCircumference() {
+		 return 2*Math.PI*radius;
+	 }
+	 
+	 public static void main(String[] args) {
+	 System.out.println("------------------------------------------------------------");
+	 System.out.println("CircleCalc v1.0");
+	 System.out.println();
+	 System.out.println("Calculates and prints information for a user-supplied radius");
+	 System.out.println("------------------------------------------------------------");
+	 
+	 System.out.print("Enter the circle's radius: ");
+	 Scanner in = new Scanner(System.in);
+	 
+	 Lab1 c = new Lab1(in.nextFloat());
+	 
+	 System.out.println("The area of the circle is = "+ c.calcArea());
+	 System.out.println("The circumference of the circle is = "+ c.calcCircumference());
+	 // TODO: compute and print the circumference and area of the circle
+	 }
 }
